723cdfacb43f826ee9c429e7df5283dd81ad9c57
Rachid Kahia <rachidkahia@tsf-444-wpa-2-080.epfl.ch>
2024-03-17 22:23:29 +0100

    le code du slave revu avec le code référence

--- slave/slave.ino
+++ slave/slave.ino
@@ -3,35 +3,51 @@
 
 // SLAVE - BOARD 2 //
 
-void setup() {
-  SPI.begin();
-  Serial.begin(9600);
-  digitalWrite(SS, HIGH);
-  SPI.setClockDivider(SPI_CLOCK_DIV8);
+char buf [100];
+volatile byte pos;
+volatile boolean process_it;
+const String response = "ECHO";
+
+void setup(void) {
+  Serial.begin(9600);   // debugging
+  SPCR |= bit(SPE);    // turn on SPI in slave mode
+  pinMode(MISO, OUTPUT);  // have to send on master in, *slave out*
+  
+  // get ready for an interrupt 
+  pos = 0;   // buffer empty
+  process_it = false;
+
+  // now turn on interrupts
+  SPI.attachInterrupt();
 }
 
-void loop() {
-  if(digitalRead(SS) == LOW) {
-    String receivedMessage = "";
-    while(true) {
-      char receivedChar = SPI.transfer(0);
-      if(receivedChar == '\0') {
-        break;
-      }
+// SPI interrupt routine
+ISR(SPI_STC_vect) {
+  byte c = SPDR;  // grab byte from SPI Data Register
+  // add to buffer if room
+  if(pos < (sizeof(buf) - 1)) {
+    buf [pos++] = c;
+  }
+  // example: newline means time to process buffer
+  if(c == '\n') {
+    process_it = true;
+  }
+}
 
-      receivedMessage += receivedChar;
-    }
-
+// main loop - wait for flag set in interrupt routine
+void loop(void) {
+  if(process_it) {
+    buf [pos] = 0;  // adds a null terminator '\0' to the end of the buffer 'buf'
+    String receivedMessage(buf);
     if(receivedMessage == "SPEAK") {
-      String response = "ECHO";
       for(char c : response) {
         SPI.transfer(c);
       }
       Serial.println(response);
-      Serial.println("MESSAGE : A SIGNAL HAS BEEN SENT TO BOARD 2 (THE SLAVE)");
+      Serial.println("MESSAGE : A SIGNAL HAS BEEN SENT TO THE BOARD 2 ");
       Serial.println("RESPONSE : OK");
     }
-  }
+    pos = 0;
+    process_it = false;
+  }  // end of flag set
 }
-
-
