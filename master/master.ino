2aaf0a079d880b261d7ec545fa3443d2bea5d1db
Rachid Kahia <rachidkahia@tsf-444-wpa-0-238.epfl.ch>
2024-03-18 20:25:34 +0100

    code revu avec struct

--- master/master.ino
+++ master/master.ino
@@ -2,8 +2,21 @@
 #include <Arduino.h>
 //#include <SPI_available.h>
 
+#define INTRANET_MAX_PAYLOAD_SIZE 256
+
 inline bool SPI_available();
 
+struct IntranetPacket {
+  uint8_t id;
+  uint8_t len;
+  uint8_t csc;
+  uint8_t payload[INTRANET_MAX_PAYLOAD_SIZE];
+};
+
+uint8_t computeCSC(const IntranetPacket& packet);
+
+const IntranetPacket message = {0x01, 4, computeCSC(message), {"SPEAK"}};
+const String message_payload = message.payload;
 
 // MASTER - BOARD 1 //
 
@@ -16,25 +29,36 @@ void setup() {
 
 void loop() {
   digitalWrite(SS, LOW);
-  String message = "SPEAK";
-  for(char c : message) {
+  for(char c : message_payload) {
     SPI.transfer(c);
   }
   digitalWrite(SS, HIGH);
 
+  Serial.println();
+  Serial.println("MESSAGE : A SIGNAL HAS BEEN SENT TO BOARD 2 (THE SLAVE)");
+
   delay(500);
 
   digitalWrite(SS, LOW);
-  while(SPI.transfer(0) != '\0') {}
+  //while(SPI.transfer(0) != '\0') {} Je sais pas si cette fonction est essentielle ou pas 
   while(SPI_available()) {
     char c = SPI.transfer(0);
     Serial.print(c);
   }
   digitalWrite(SS, HIGH);
 
-  Serial.println();
-  Serial.println("MESSAGE : A SIGNAL HAS BEEN SENT TO BOARD 2 (THE SLAVE)");
   delay(1000);
+}
+
+// fonctions :
+uint8_t computeCSC(const IntranetPacket& packet) {
+  uint8_t checksum = 0;
+  checksum += packet.id;
+  checksum += packet.len;
+  for(int i = 0; i < packet.len; ++i) {
+    checksum += packet.payload[i];
+  }
+  return checksum;
 }
 
 inline bool SPI_available() {
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,12 +1,30 @@
 #include <SPI.h>
 #include <Arduino.h>
 
+#define INTRANET_MAX_PAYLOAD_SIZE 256
+
 // SLAVE - BOARD 2 //
+
+struct IntranetPacket {
+  uint8_t id;
+  uint8_t len;
+  uint8_t csc;
+  uint8_t payload[INTRANET_MAX_PAYLOAD_SIZE];
+};
+
+uint8_t computeCSC(const IntranetPacket& packet);
 
 char buf [100];
 volatile byte pos;
 volatile boolean process_it;
-const String response = "ECHO";
+
+const IntranetPacket response = {0x02, 4, computeCSC(response), {"ECHO"}};
+const String response_payload = response.payload;
+
+//response.id = 0x02;
+//response.len = 4;
+//response.payload = {'ECHO'};
+//response.csc = computeCSC(response);
 
 void setup(void) {
   Serial.begin(9600);   // debugging
@@ -40,14 +58,25 @@ void loop(void) {
     buf [pos] = 0;  // adds a null terminator '\0' to the end of the buffer 'buf'
     String receivedMessage(buf);
     if(receivedMessage == "SPEAK") {
-      for(char c : response) {
+      for(char c : response.payload) {
         SPI.transfer(c);
       }
-      Serial.println(response);
+      Serial.println(response_payload);
       Serial.println("MESSAGE : A SIGNAL HAS BEEN SENT TO THE BOARD 2 ");
       Serial.println("RESPONSE : OK");
     }
     pos = 0;
     process_it = false;
   }  // end of flag set
+}
+
+// fonctions :
+uint8_t computeCSC(const IntranetPacket& packet) {
+  uint8_t checksum = 0;
+  checksum += packet.id;
+  checksum += packet.len;
+  for(int i = 0; i < packet.len; ++i) {
+    checksum += packet.payload[i];
+  }
+  return checksum;
 }
